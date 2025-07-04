53887047c36902beda23bf50195002fa3169f5b2
akkayasoft <akkayasoft@gmail.com>
2025-07-04 15:14:04 +0300

    ders2 bitti

--- javascript/js/ders1.js
+++ javascript/js/ders1.js
@@ -1,3 +1,8 @@
 alert("Harici sayfa dosyasından gelen mesaj");
 console.log("Harici sayfa dosyasından gelen mesaj");
 document.write("Harici sayfa dosyasından gelen mesaj");
+
+document.getElementById("baslik").style.fontSize="52px";
+document.getElementById("icerik").innerHTML="<b>Bu yazı kalın bir yazıdır.</b>";
+document.getElementById("paragraf").innerText="Paragrafa yeni cümleler eklenebilir.";
+document.getElementById("adSoyad").value="Mustafa ÖZER";
